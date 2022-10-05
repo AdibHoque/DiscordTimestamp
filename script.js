ea5c6f14bfcc3b27972275304300a2103e96b046
Glitch (glitch-hello-website) <none>
2022-10-05 13:26:22 +0000

    🎨🏠 Checkpoint ./index.html:3244687/312 ./script.js:3244687/42

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+function myfun() 
+{
 var desiredwr=document.getElementById('dwr').value;
 var currentwr=document.getElementById('cwr').value;
 var matches=document.getElementById('ma').value;
 
-var calc=matches*(desiredwr-currentwr)/(100-desiredwr)
+var calc=Math.round(matches*(desiredwr-currentwr)/(100-desiredwr))
 
 document.getElementById('result').innerHTML=calc
+}
