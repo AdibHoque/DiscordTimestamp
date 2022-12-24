13d034fa2c01e7f772ba62ea841d36059ffba0be
Glitch (glitch-hello-website) <none>
2022-12-24 15:05:22 +0000

    💐🏪 Checkpoint ./script.js:3244687/286

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function myfun() 
 {
-var time=document.getElementById('time').value;
+var time = document.getElementById('time').value;
 var date=document.getElementById('date').value;
+var datetime = Date.now()
+var timeobj = new Date(datetime)
 var type=document.getElementById('type').value;
 
-var v=`${} ${} ${}`
+var v=`${time} ${date} ${type}`
 
-document.getElementById('result').innerHTML=calc
+document.getElementById('result').innerHTML=datetime
 }
